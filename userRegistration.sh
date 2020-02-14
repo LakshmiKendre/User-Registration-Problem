ed91c71857fb41f065e830fd97e83cf8952e56b2
LakshmiKendre <lakshmi.patil.nine@gmail.com>
2020-02-14 12:30:40 +0530

    Added: feature to Valid Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
 
+shopt -s extglob
 echo "Welcome to User Registration "
 echo -e "Enter Input : \c"
 read input
 
-firstName="^[A-Z]{1}[a-zA-z]{2,}$"
-if [[ $input =~ $firstName ]]
+firstName="^[A-Z]{1}[a-z]{2,}$"
+lastName="^[A-Z][a-z]{2,}$"
+
+if [[ $input =~ $lastName ]]
 then
 	echo "Valid"
 else
