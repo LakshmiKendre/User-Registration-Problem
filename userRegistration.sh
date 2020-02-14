0ac57d3c1cf8814bef20f664b6b0ac21fdd0fea1
LakshmiKendre <lakshmi.patil.nine@gmail.com>
2020-02-14 11:28:04 +0530

    Added: feature to Valid First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration "
+echo -e "Enter Input : \c"
+read input
 
+firstName="^[A-Z]{1}[a-zA-z]{2,}$"
+if [[ $input =~ $firstName ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
